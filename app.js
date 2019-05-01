ee8a60d85d8db1742041e6ac0443247e2b9b906b
mstill3 <stillwell006@gmail.com>
2019-05-01 16:52:13 -0400

    clean

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 
-var express = require('express');
-var app = express();
 
-var googlefusion = require('./api/googlefusion');
-app.use('/api', googlefusion);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 
-var app = require('./app');
-var port = process.env.PORT || 80;
+var express = require('express');
+var app = express();
+var googlefusion = require('./api/googlefusion');
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+app.use('/api', googlefusion);
+app.listen(port, function() 
+{
+    console.log('Express server listening on port ' + process.env.PORT || 80);
 });
